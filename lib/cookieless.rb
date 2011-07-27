ee1dd6a56d55d3616bc63cfc2b083ab2860e3a6a
Jinzhu <wosmvp@gmail.com>
2011-07-27 18:55:24 +0800

    fix some bugs

--- lib/cookieless.rb
+++ lib/cookieless.rb
@@ -7,23 +7,26 @@ module Rack
     end
 
     def cache_store
-      options[:cache_store] || Rails.cache
+      @options[:cache_store] || Rails.cache
     end
 
     def session_key
-      (options[:session_id] || :session_id).to_s
+      (@options[:session_id] || :session_id).to_s
     end
 
     def call(env)
       if support_cookie?(env)
         @app.call(env)
       else
-        session_id = Rack::Utils.parse_query(env["QUERY_STRING"], "&")[session_key] || env["session_id"]
-        cache_id = Digest::SHA1.hexdigest session_id
+        session_id = Rack::Utils.parse_query(env["QUERY_STRING"], "&")[session_key].to_s
 
-        env["rack.session"] = cache_store.fetch(cache_id) { env["rack.session"] }
+        cache_id = Digest::SHA1.hexdigest(session_id + env["HTTP_USER_AGENT"] + env["REMOTE_ADDR"])
+
+        env["rack.session"] = cache_store.fetch(cache_id) { env["rack.session"] }.dup if session_id.present?
 
         status, header, body = @app.call(env)
+
+        # session_id = env["rack.session"]["session_id"]
 
         body = process_body(body, session_id)
         cache_store.write(cache_id, env["rack.session"])
@@ -34,6 +37,7 @@ module Rack
 
     def process_body(body, session_id)
       ###
+      body
     end
 
     def support_cookie?(env)
