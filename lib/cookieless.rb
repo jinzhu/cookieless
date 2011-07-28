374a07899e09c6119c8753bdbc5eca9fa69aa0fb
Jinzhu <wosmvp@gmail.com>
2011-07-28 12:27:00 +0800

    fix session_id haven't been changed for different users

--- lib/cookieless.rb
+++ lib/cookieless.rb
@@ -2,7 +2,6 @@ require 'digest/sha1'
 
 module Rack
   class Cookieless
-    attr_accessor :session_id, :cache_id
     def initialize(app, options={})
       @app, @options = app, options
     end
@@ -19,29 +18,28 @@ module Rack
       if support_cookie?(env)
         @app.call(env)
       else
-        set_session_id(Rack::Utils.parse_query(env["QUERY_STRING"], "&")[session_key].to_s, env)
-        env["rack.session"].update(cache_store.fetch(@cache_id) { env["rack.session"] }) if @cache_id
+        session_id, cache_id = set_session_id(Rack::Utils.parse_query(env["QUERY_STRING"], "&")[session_key].to_s, env)
+        env["rack.session"].update(cache_store.fetch(cache_id) { env["rack.session"] }) if cache_id
 
         status, header, response = @app.call(env)
 
-        set_session_id(env["rack.session"]["session_id"], env) if @session_id.blank?
-        cache_store.write(@cache_id, env["rack.session"].to_hash)
+        session_id, cache_id = set_session_id(env["rack.session"]["session_id"], env) if session_id.blank?
+        cache_store.write(cache_id, env["rack.session"].to_hash)
 
-        response.body = process_body(response.body) if response.respond_to?(:body)
+        response.body = process_body(response.body, session_id) if response.respond_to?(:body)
         [status, header, response]
       end
     end
 
     def set_session_id(session_id, env)
-      @session_id = session_id
-      @cache_id = Digest::SHA1.hexdigest(session_id + env["HTTP_USER_AGENT"] + env["REMOTE_ADDR"]) if @session_id.present?
+      [session_id, (session_id.present? ? Digest::SHA1.hexdigest(session_id + env["HTTP_USER_AGENT"] + env["REMOTE_ADDR"]) : nil)]
     end
 
-    def process_body(body)
+    def process_body(body, session_id)
       body_doc = Nokogiri::HTML(body)
       #TODO: change hardcode "?session_id"
-      body_doc.css("a").map { |a| a["href"] += "?#{session_key}=#{@session_id}" if a["href"] }
-      body_doc.css("form").map { |form| form["action"] += "?#{session_key}=#{@session_id}" if form["action"] }
+      body_doc.css("a").map { |a| a["href"] += "?#{session_key}=#{session_id}" if a["href"] }
+      body_doc.css("form").map { |form| form["action"] += "?#{session_key}=#{session_id}" if form["action"] }
       body_doc.to_html
     end
 
