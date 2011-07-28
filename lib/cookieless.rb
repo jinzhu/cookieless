8e036c7dcbd97749a0fe64f3d4c565982dca004d
binku <binku87@gmail.com>
2011-07-28 10:51:31 +0800

    implement process_body

--- lib/cookieless.rb
+++ lib/cookieless.rb
@@ -25,23 +25,30 @@ module Rack
           env["rack.session"].update(cache_store.fetch(cache_id) { env["rack.session"] })
         end
 
-        status, header, body = @app.call(env)
+        status, header, response = @app.call(env)
 
         if session_id.blank?
           session_id = env["rack.session"]["session_id"]
           cache_id = Digest::SHA1.hexdigest(session_id + env["HTTP_USER_AGENT"] + env["REMOTE_ADDR"])
         end
 
-        body = process_body(body, session_id)
+        response.body = process_body(response.body, session_id)
         cache_store.write(cache_id, env["rack.session"].to_hash)
 
-        [status, header, body]
+        [status, header, response]
       end
     end
 
     def process_body(body, session_id)
-      ###
-      body
+      body_doc = Nokogiri::HTML(body)
+      #TODO: change hardcode "?_session_id"
+      body_doc.css("a").map { |a| a["href"] += "?_session_id=#{session_id}" if a["href"] }
+      body_doc.css("form").map do |form|
+        session_id_input = Nokogiri::XML::Node.new "input",body_doc
+        [["name","_session_id"], ["value", session_id], ["type", "hidden"]].map { |attr, value| session_id_input[attr] = value }
+        form.add_child(session_id_input)
+      end
+      body_doc.to_html
     end
 
     def support_cookie?(env)
