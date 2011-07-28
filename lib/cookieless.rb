c63fc2bf805a4d5560f4fc10586a4959b575589a
Jinzhu <wosmvp@gmail.com>
2011-07-28 15:04:23 +0800

    only process html body

--- lib/cookieless.rb
+++ lib/cookieless.rb
@@ -1,4 +1,5 @@
 require 'digest/sha1'
+require 'uri'
 
 module Rack
   class Cookieless
@@ -19,6 +20,7 @@ module Rack
         @app.call(env)
       else
         session_id, cache_id = set_session_id(Rack::Utils.parse_query(env["QUERY_STRING"], "&")[session_key].to_s, env)
+        session_id, cache_id = set_session_id(Rack::Utils.parse_query((URI.parse(env['HTTP_REFERER']).query rescue nil))[session_key].to_s, env) if session_id.blank?
         env["rack.session"].update(cache_store.fetch(cache_id) { env["rack.session"] }) if cache_id
 
         status, header, response = @app.call(env)
@@ -26,7 +28,11 @@ module Rack
         session_id, cache_id = set_session_id(env["rack.session"]["session_id"], env) if session_id.blank?
         cache_store.write(cache_id, env["rack.session"].to_hash)
 
-        response.body = process_body(response.body, session_id) if response.respond_to?(:body)
+
+        if env['action_dispatch.request.path_parameters'][:format].to_s =~ /\A(html)?\Z/
+          response.body = process_body(response.body, session_id) if response.respond_to?(:body)
+        end
+
         [status, header, response]
       end
     end
